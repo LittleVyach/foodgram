9289ba489114cc255beed7761f47f0b39b0b0b54
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-22 14:17:02 +0500

    Исправил баг

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.228.241', 'foodgramcat.hopto.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.228.241']
 
 
 INSTALLED_APPS = [
