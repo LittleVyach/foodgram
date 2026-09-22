4291fe7eb3abb860b3fdb902518a65ba66858929
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-22 16:17:23 +0500

    Добавил миграцию

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -62,6 +62,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
+        related_name='recipes',
         verbose_name='Автор рецепта'
     )
     name = models.CharField(
