221a13953777650af3b46429372bcc90ad2311d4
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-23 10:00:25 +0500

    добавил настройки пользователя

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -128,6 +128,7 @@ DJOSER = {
     'SERIALIZERS': {
         'user': 'api.serializers.UserSerializer',
         'current_user': 'api.serializers.UserSerializer',
+        'user_create': 'api.serializers.CustomUserCreateSerializer',
     },
     'PERMISSIONS': {
         'user': ['rest_framework.permissions.AllowAny'],
