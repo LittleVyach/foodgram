6a2a8a0878b6d50c8943550636be894ac113a694
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-23 11:48:32 +0500

    добавил для избранного

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -97,8 +97,8 @@ class RecipeReadSerializer(serializers.ModelSerializer):
     ingredients = ReadRecipeIngredientSerializer(
         source='recipe_ingredients', many=True, read_only=True
     )
-    is_favorited = serializers.BooleanField(default=False)
-    is_in_shopping_cart = serializers.BooleanField(default=False)
+    is_favorited = serializers.SerializerMethodField(read_only=True)
+    is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
     image = Base64ImageField()
 
     class Meta:
@@ -115,6 +115,19 @@ class RecipeReadSerializer(serializers.ModelSerializer):
             'text',
             'cooking_time',
         )
+
+    def get_is_favorited(self, obj):
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
+            return False
+        return Favorite.objects.filter(user=request.user, recipe=obj).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
+            return False
+        return ShoppingCart.objects.filter(
+            user=request.user, recipe=obj).exists()
 
 
 class RecipeIngredientWriteSerializer(serializers.ModelSerializer):
