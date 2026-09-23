3403c397c207c353b5a84d199177d5a07c6c48b5
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-23 14:30:43 +0500

    изменил фильтрацию

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -25,11 +25,16 @@ class RecipeFilter(django_filters.FilterSet):
         fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            recipe_ids = Favorite.objects.filter(
-                user=self.request.user).values_list('recipe_id', flat=True)
-            return queryset.filter(id__in=recipe_ids)
-        return queryset
+        if not value:
+            return queryset
+
+        request = getattr(self, 'request', None)
+        if not request or not request.user.is_authenticated:
+            return queryset.none()
+
+        recipe_ids = Favorite.objects.filter(
+            user=request.user).values_list('recipe_id', flat=True)
+        return queryset.filter(id__in=recipe_ids)
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and self.request.user.is_authenticated:
