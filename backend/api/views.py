9d36365feba1f1db7185ec531c1e60dd6b60e9b7
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-25 18:28:56 +0500

    убрал лишний импорт

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,10 +9,10 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from recipes.models import (Favorite,
-                            Follow,  # noqa: F401
-                            Ingredient, Recipe,
-                            RecipeIngredient, ShoppingCart, Tag)
+from recipes.models import (
+    Favorite, Ingredient, Recipe,
+    RecipeIngredient, ShoppingCart, Tag
+)
 
 from .filters import IngredientSearchFilter, RecipeFilter
 from .pagination import LimitPageNumberPagination
