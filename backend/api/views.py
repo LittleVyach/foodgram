1ece5e7e7965b7949d088105428d9794a3f4d954
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-25 18:33:43 +0500

    исправил отступы

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,14 +9,8 @@ from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from recipes.models import (
-    Favorite,
-    Ingredient,
-    Recipe,
-    RecipeIngredient,
-    ShoppingCart,
-    Tag,
-)
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingCart, Tag)
 
 from .filters import IngredientSearchFilter, RecipeFilter
 from .pagination import LimitPageNumberPagination
