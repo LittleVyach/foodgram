8043e239d00d9f121546013cefa823110d5be262
Vyacheslav Pak <ssilent301@gmail.com>
2026-09-27 00:09:24 +0500

    удалил лишний импорт 7

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django_filters.rest_framework import DjangoFilterBackend
-from djoser.serializers import UserSerializer as DjoserBaseUserSerializer
 from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
